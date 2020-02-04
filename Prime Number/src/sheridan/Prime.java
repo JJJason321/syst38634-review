d9b549c4903fe569c97076ef86851260e982e108
ramsesmindx <ramses@mindextent.com>
2020-02-04 01:31:01 -0500

    Initial version

---
+++ Prime Number/src/sheridan/Prime.java
@@ -0,0 +1,23 @@
+package sheridan;
+
+public class Prime {
+	
+	
+	public static boolean isPrime( int number ) {
+		for ( int i = 2 ; i <= number /2 ; i ++) {
+			if ( number % i == 0  ) {
+				return false;
+			}
+		}
+		return true;
+ 	}
+
+	public static void main(String[] args) {
+		
+
+		System.out.println( "Number 29 is prime? " + Prime.isPrime( 29 ) );
+		System.out.println( "Number 33 is prime? " + Prime.isPrime( 33 ) );
+		
+	}
+
+}
